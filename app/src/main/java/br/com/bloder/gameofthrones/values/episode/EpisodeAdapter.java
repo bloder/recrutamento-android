02363e13e13a50c7663bb1ee4ac2fe18ac1b4744
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:24:21 -0300

    Implements racycler adapter functions

--- app/src/main/java/br/com/bloder/gameofthrones/values/episode/EpisodeAdapter.java
+++ app/src/main/java/br/com/bloder/gameofthrones/values/episode/EpisodeAdapter.java
@@ -1,7 +1,33 @@
 package br.com.bloder.gameofthrones.values.episode;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by bloder on 04/06/16.
  */
-public class EpisodeAdapter {
+public class EpisodeAdapter extends RecyclerView.Adapter<EpisodeAdapter.InternalViewHolder> {
+
+
+  @Override
+  public InternalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override
+  public void onBindViewHolder(InternalViewHolder holder, int position) {
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return 0;
+  }
+
+  public class InternalViewHolder extends RecyclerView.ViewHolder {
+    public InternalViewHolder(View itemView) {
+      super(itemView);
+    }
+  }
 }
