f7c0d793b09c759ab244a8a362760a6821d792f0
Daniel <wowmegadenis@hotmail.com>
2016-06-04 10:39:55 -0300

    Create retrofit builder

--- app/src/main/java/br/com/bloder/gameofthrones/api/TraktApi.java
+++ app/src/main/java/br/com/bloder/gameofthrones/api/TraktApi.java
@@ -1,7 +1,29 @@
 package br.com.bloder.gameofthrones.api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import br.com.bloder.gameofthrones.BuildConfig;
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+
 /**
  * Created by bloder on 04/06/16.
  */
 public class TraktApi {
+
+  public static TraktServices getServices() {
+
+    Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            .create();
+
+    RestAdapter retrofit = new RestAdapter.Builder()
+            .setEndpoint(BuildConfig.END_POINT)
+            .setLogLevel(RestAdapter.LogLevel.FULL)
+            .setConverter(new GsonConverter(gson))
+            .build();
+
+    return retrofit.create(TraktServices.class);
+  }
 }
