252d136c4950bbc8262965c461235e5ea9bb0b88
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:04:16 -0300

    Add episodes implementation

---
+++ app/src/main/java/br/com/bloder/gameofthrones/repository/production/ProductionEpisodesRepository.java
@@ -0,0 +1,17 @@
+package br.com.bloder.gameofthrones.repository.production;
+
+import java.util.List;
+
+import br.com.bloder.gameofthrones.repository.resources.EpisodesRepository;
+import br.com.bloder.gameofthrones.values.episode.Episode;
+
+/**
+ * Created by bloder on 04/06/16.
+ */
+public class ProductionEpisodesRepository implements EpisodesRepository {
+  
+  @Override
+  public List<Episode> getEpisodes() {
+    return null;
+  }
+}
