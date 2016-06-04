ab7cf6f7f1dc8d02df3f6fc42a4d21d0adcb7ad0
Daniel <wowmegadenis@hotmail.com>
2016-06-04 10:43:34 -0300

    Create episode payload object to get ws episodes

--- app/src/main/java/br/com/bloder/gameofthrones/api/payloads/EpisodePayload.java
+++ app/src/main/java/br/com/bloder/gameofthrones/api/payloads/EpisodePayload.java
@@ -1,7 +1,17 @@
 package br.com.bloder.gameofthrones.api.payloads;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by bloder on 04/06/16.
  */
 public class EpisodePayload {
+
+  @SerializedName("number") private final int number;
+  @SerializedName("title") private final String title;
+
+  public EpisodePayload(int number, String title) {
+    this.number = number;
+    this.title = title;
+  }
 }
