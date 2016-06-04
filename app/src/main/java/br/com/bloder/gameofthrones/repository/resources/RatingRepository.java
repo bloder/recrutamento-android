3ad901e28cb07e4557fd7f65d1a9e71c29cb8562
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:01:30 -0300

    Implement resources interfaces

--- app/src/main/java/br/com/bloder/gameofthrones/repository/resources/RatingRepository.java
+++ app/src/main/java/br/com/bloder/gameofthrones/repository/resources/RatingRepository.java
@@ -1,7 +1,11 @@
 package br.com.bloder.gameofthrones.repository.resources;
 
+import br.com.bloder.gameofthrones.values.rating.Rating;
+
 /**
  * Created by bloder on 04/06/16.
  */
 public interface RatingRepository {
+
+  Rating getRating();
 }
