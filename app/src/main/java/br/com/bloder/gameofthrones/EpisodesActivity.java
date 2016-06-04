86d93f331334625479f3a7670276f0d232de3fec
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:22:04 -0300

    Add recycler view to episodes layout

--- app/src/main/java/br/com/bloder/gameofthrones/EpisodesActivity.java
+++ app/src/main/java/br/com/bloder/gameofthrones/EpisodesActivity.java
@@ -1,11 +1,10 @@
 package br.com.bloder.gameofthrones;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import org.androidannotations.annotations.EActivity;
 
-@EActivity(R.layout.activity_episodes)
+@EActivity(R.layout.episodes)
 public class EpisodesActivity extends AppCompatActivity {
 
 }
