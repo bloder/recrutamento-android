5bfe92002e1c82fad023d5a6b4a4f19677186a2a
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:09:13 -0300

    Create logic to return episode pojo list

--- app/src/main/java/br/com/bloder/gameofthrones/repository/production/ProductionEpisodesRepository.java
+++ app/src/main/java/br/com/bloder/gameofthrones/repository/production/ProductionEpisodesRepository.java
@@ -1,9 +1,13 @@
 package br.com.bloder.gameofthrones.repository.production;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import br.com.bloder.gameofthrones.api.TraktApi;
+import br.com.bloder.gameofthrones.api.payloads.EpisodePayload;
 import br.com.bloder.gameofthrones.repository.resources.EpisodesRepository;
 import br.com.bloder.gameofthrones.values.episode.Episode;
+import retrofit.RetrofitError;
 
 /**
  * Created by bloder on 04/06/16.
@@ -12,6 +16,19 @@ public class ProductionEpisodesRepository implements EpisodesRepository {
 
   @Override
   public List<Episode> getEpisodes() {
-    return null;
+    try {
+      return toEpisodes(TraktApi.getServices().getEpisodes());
+    } catch (RetrofitError ignored) {
+      return new ArrayList<>();
+    }
+  }
+
+  public List<Episode> toEpisodes(List<EpisodePayload> episodePayloads) {
+    List<Episode> episodes = new ArrayList<>();
+
+    for(EpisodePayload episodePayload : episodePayloads) {
+      episodes.add(episodePayload.toEpisode());
+    }
+    return episodes;
   }
 }
