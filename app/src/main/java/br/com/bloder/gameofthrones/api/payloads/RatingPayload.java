44556b55890b0ca24e615e22ae527780c0a51868
Daniel <wowmegadenis@hotmail.com>
2016-06-04 10:45:16 -0300

    Create rating payload object to get ws rating

--- app/src/main/java/br/com/bloder/gameofthrones/api/payloads/RatingPayload.java
+++ app/src/main/java/br/com/bloder/gameofthrones/api/payloads/RatingPayload.java
@@ -1,7 +1,15 @@
 package br.com.bloder.gameofthrones.api.payloads;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by bloder on 04/06/16.
  */
 public class RatingPayload {
+
+  @SerializedName("rating") private final double rating;
+
+  public RatingPayload(double rating) {
+    this.rating = rating;
+  }
 }
