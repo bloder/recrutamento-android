14088df933a0ed87292760d4995b83f954d34971
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:02:19 -0300

    Make production repo fac implements repo fac

--- app/src/main/java/br/com/bloder/gameofthrones/repository/production/ProductionRepositoryFactory.java
+++ app/src/main/java/br/com/bloder/gameofthrones/repository/production/ProductionRepositoryFactory.java
@@ -1,7 +1,21 @@
 package br.com.bloder.gameofthrones.repository.production;
 
+import br.com.bloder.gameofthrones.repository.RepositoryFactory;
+import br.com.bloder.gameofthrones.repository.resources.EpisodesRepository;
+import br.com.bloder.gameofthrones.repository.resources.RatingRepository;
+
 /**
  * Created by bloder on 04/06/16.
  */
-public class ProductionRepositoryFactory {
+public class ProductionRepositoryFactory implements RepositoryFactory {
+  
+  @Override
+  public EpisodesRepository forEpisodes() {
+    return null;
+  }
+
+  @Override
+  public RatingRepository forRating() {
+    return null;
+  }
 }
