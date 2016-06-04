b361e523316ac1e629cafe37d532dda7f242ec1a
Daniel <wowmegadenis@hotmail.com>
2016-06-04 10:49:03 -0300

    Add rating pojo

---
+++ app/src/main/java/br/com/bloder/gameofthrones/values/rating/Rating.java
@@ -0,0 +1,13 @@
+package br.com.bloder.gameofthrones.values.rating;
+
+/**
+ * Created by bloder on 04/06/16.
+ */
+public class Rating {
+
+  public final double rating;
+
+  public Rating(double rating) {
+    this.rating = rating;
+  }
+}
