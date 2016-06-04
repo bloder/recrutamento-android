43d0891b4ae53b3e770b331fd324765fb8a4b89a
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:06:29 -0300

    Add rating implementation class

---
+++ app/src/main/java/br/com/bloder/gameofthrones/repository/production/ProductionRatingRepository.java
@@ -0,0 +1,15 @@
+package br.com.bloder.gameofthrones.repository.production;
+
+import br.com.bloder.gameofthrones.repository.resources.RatingRepository;
+import br.com.bloder.gameofthrones.values.rating.Rating;
+
+/**
+ * Created by bloder on 04/06/16.
+ */
+public class ProductionRatingRepository implements RatingRepository {
+
+  @Override
+  public Rating getRating() {
+    return null;
+  }
+}
