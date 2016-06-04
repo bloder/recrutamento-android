7c3965bb4d12c85a886813c5dff74bcdffd86148
Daniel <wowmegadenis@hotmail.com>
2016-06-04 10:59:22 -0300

    Implements repository factory with content methods

--- app/src/main/java/br/com/bloder/gameofthrones/repository/RepositoryFactory.java
+++ app/src/main/java/br/com/bloder/gameofthrones/repository/RepositoryFactory.java
@@ -1,7 +1,13 @@
 package br.com.bloder.gameofthrones.repository;
 
+import br.com.bloder.gameofthrones.repository.resources.EpisodesRepository;
+import br.com.bloder.gameofthrones.repository.resources.RatingRepository;
+
 /**
  * Created by bloder on 04/06/16.
  */
 public interface RepositoryFactory {
+
+  EpisodesRepository forEpisodes();
+  RatingRepository forRating();
 }
