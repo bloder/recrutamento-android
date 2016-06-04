ab7f97702f181593a2996852aa781ae1ba44fe79
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:34:30 -0300

    Make relationship between adapter and view holder

--- app/src/main/java/br/com/bloder/gameofthrones/values/episode/EpisodeAdapter.java
+++ app/src/main/java/br/com/bloder/gameofthrones/values/episode/EpisodeAdapter.java
@@ -1,33 +1,46 @@
 package br.com.bloder.gameofthrones.values.episode;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * Created by bloder on 04/06/16.
  */
 public class EpisodeAdapter extends RecyclerView.Adapter<EpisodeAdapter.InternalViewHolder> {
 
+  private Context context;
+  private List<Episode> episodes;
+
+  public EpisodeAdapter(Context context, List<Episode> episodes) {
+    this.context = context;
+    this.episodes = episodes;
+  }
 
   @Override
   public InternalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return null;
+    return new InternalViewHolder();
   }
 
   @Override
   public void onBindViewHolder(InternalViewHolder holder, int position) {
-
+    holder.bind(this.episodes.get(position));
   }
 
   @Override
   public int getItemCount() {
-    return 0;
+    return episodes.size();
   }
 
   public class InternalViewHolder extends RecyclerView.ViewHolder {
-    public InternalViewHolder(View itemView) {
-      super(itemView);
+    public InternalViewHolder() {
+      super(EpisodeViewHolder_.build(context));
+    }
+
+    public void bind(Episode episode) {
+      ((EpisodeViewHolder) this.itemView).bind(episode);
     }
   }
 }
