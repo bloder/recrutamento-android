5b8399f56245b0426d45763c1589a27b337ed74c
Daniel <wowmegadenis@hotmail.com>
2016-06-04 11:30:51 -0300

    Add episode view holder implementation

--- app/src/main/java/br/com/bloder/gameofthrones/values/episode/EpisodeViewHolder.java
+++ app/src/main/java/br/com/bloder/gameofthrones/values/episode/EpisodeViewHolder.java
@@ -1,7 +1,30 @@
 package br.com.bloder.gameofthrones.values.episode;
 
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
+
+import br.com.bloder.gameofthrones.R;
+
 /**
  * Created by bloder on 04/06/16.
  */
-public class EpisodeViewHolder {
+@EViewGroup(R.layout.episode)
+public class EpisodeViewHolder extends LinearLayout {
+
+  @ViewById
+  protected TextView title;
+  @ViewById protected TextView episodeNumber;
+
+  public EpisodeViewHolder(Context context) {
+    super(context);
+  }
+
+  public void bind(Episode episode) {
+    title.setText(episode.title);
+    episodeNumber.setText("E" + String.valueOf(episode.number));
+  }
 }
